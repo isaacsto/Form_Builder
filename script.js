529473c3d1f05c42fc904d19a9c98d441800da9c
Isaac Stofko <istofko@contractornation.com>
2023-10-11 10:26:58 -0400

    built out form

--- script.js
+++ script.js
@@ -9,13 +9,25 @@
             // Get values of input fields
             const formTitle = document.querySelector('#form-title').value;
             const firstLabel = document.querySelector('#first-label').value;
+            const firstClass = document.querySelector('#first-class').value;
+            const secondLabel = document.querySelector('#second-label').value;
+            const secondClass = document.querySelector('#second-class').value;
+            const thirdLabel = document.querySelector('#third-label').value;
+            const thirdClass = document.querySelector('#third-class').value;
+            const buttonLabel = document.querySelector('#button-label').value;
+            const buttonType = document.querySelector('#button-type').value;
 
             // Construct HTML string
             const html = `
                 <form>
                     <h2>${formTitle}</h2>
-                    <label>${firstLabel}</label>
+                    <label class="${firstClass}">${firstLabel}</label>
                     <input type="text" id="generated-input">
+                    <label class="${secondClass}">${secondLabel}</label>
+                    <input type="text" id="generated-input">
+                    <label class="${thirdClass}">${thirdLabel}</label>
+                    <input type="text" id="generated-input">
+                    <button type="${buttonType}">${buttonLabel}</button>
                 </form>
             `;
 
