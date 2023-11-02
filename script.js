2f90c8852e66d1c2b07319d9322c87e21dcd57fa
Isaac Stofko <istofko@contractornation.com>
2023-11-02 16:26:40 -0400

    added type

--- script.js
+++ script.js
@@ -44,26 +44,31 @@ rowElement.insertAdjacentHTML('beforeend', newRow);
             const formId = document.querySelector('#form-id').value;
 
             const firstClass = document.querySelector('#class-one').value;
+            const firstType = document.querySelector('#type-one').value;
             const firstLabel = document.querySelector('#label-one').value;
             const firstName = document.querySelector('#name-one').value;
             const firstId = document.querySelector('#id-one').value;
           
             const secondClass = document.querySelector('#class-two').value;
+            const secondType = document.querySelector('#type-two').value;
             const secondLabel = document.querySelector('#label-two').value;
             const secondName = document.querySelector('#name-two').value;
             const secondId = document.querySelector('#id-two').value;
 
             const thirdLabel = document.querySelector('#label-three').value;
+            const thirdType = document.querySelector('#type-three').value;
             const thirdClass = document.querySelector('#class-three').value;
             const thirdName = document.querySelector('#name-three').value;
             const thirdId = document.querySelector('#id-three').value;
 
             const fourthLabel = document.querySelector('#label-four').value;
+            const fourthType = document.querySelector('#type-four').value;
             const fourthClass = document.querySelector('#class-four').value;
             const fourthName = document.querySelector('#name-four').value;
             const fourthId = document.querySelector('#id-four').value;
 
             const buttonLabel = document.querySelector('#button-label').value;
+            const buttonAction = document.querySelector('#button-action').value;
             const buttonType = document.querySelector('#button-type').value;
             const buttonClass = document.querySelector('#button-class').value;
             const buttonId = document.querySelector('#button-id').value;
@@ -74,18 +79,18 @@ rowElement.insertAdjacentHTML('beforeend', newRow);
                     <h2>${formTitle}</h2>
 
                     <label class="${firstClass}" name="${firstName}" id="${firstId}">${firstLabel}</label>
-                    <input type="text" id="generated-input">
+                    <input type="${firstType}" id="generated-input">
                     
                     <label class="${secondClass}" name="${secondName}" id="${secondId}">${secondLabel}</label>
-                    <input type="text" id="generated-input">
+                    <input type="${secondType}" id="generated-input">
                     
                     <label class="${thirdClass}" name="${thirdName}" id="${thirdId}">${thirdLabel}</label>
-                    <input type="text" id="generated-input">
+                    <input type="${thirdType}" id="generated-input">
 
                     <label class="${fourthClass}" name="${fourthName}" id="${fourthId}">${fourthLabel}</label>
-                    <input type="text" id="generated-input">
+                    <input type="${fourthType}" id="generated-input">
                    
-                    <button type="${buttonType}" class="${buttonClass}"id="${buttonId}"">${buttonLabel}</button>
+                    <button type="${buttonType}" class="${buttonClass}"id="${buttonId}" action="${buttonAction}">${buttonLabel}</button>
                 </form>
             `;
 
